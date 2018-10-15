3f42052494fbfa5b4b725e79bff42a1074704d4c
Sora0Siro <43938296+Sora0Siro@users.noreply.github.com>
2018-10-15 23:47:58 +0300

    *Check for non existing words  - Add new words  - Add states for faster search *Check for existing words

--- src/Automat.java
+++ src/Automat.java
@@ -7,15 +7,20 @@ public class Automat
     private ArrayList<ArrayList<Character>> words;
     private ArrayList<ArrayList<Integer>> stateTransition;
     private boolean wordExist = true;
-    Scanner sc = new Scanner(System.in);
-    String glTmp = "";
-    char[] glTmpCh;
-    boolean cont = false;
+    private Scanner sc = new Scanner(System.in);
+    private String glTmp = "";
+    private char[] glTmpCh;
+    private boolean cont = false;
+
+    private int iterWo = 0;
+    private int iterCh = 0;
+    private int steps = 0;
 
     public Automat()
     {
         words = new ArrayList<>();
         stateTransition = new ArrayList<>();
+        //
         do{
         enterWord();
         checkSequence();
@@ -23,6 +28,8 @@ public class Automat
             String tmp = sc.next();
             if(tmp.toCharArray()[0]=='Y' || tmp.toCharArray()[0]=='y') cont=true;
             else cont = false;
+
+           lineI();
         }while(cont);
     }
 
@@ -36,71 +43,137 @@ public class Automat
 
     private void checkSequence()
     {
-        if(!words.isEmpty())
+        steps = 0;
+        int state = 0;//номер проверяем. ячейки
+        iterCh = 0;
+        if(!words.isEmpty())//если словарь не пустой
         {
-            int iterWo = 0;
-            int iterCh = 0;
-            //если введенная буква совпадает с имеющимся словом
-            for(char c:glTmpCh)
+            for(char c: glTmpCh)//каждый символ в введенном слове
             {
-                if (c == words.get(iterWo).get(iterCh))
+                if(c==words.get(state).get(iterCh))
                 {
-                    iterCh++;
-                    wordExist = true;
+                    wordExist = true;//проверяемые буквы одинаковы
+                    iterCh++; //итерация для след. шага
+                    steps++;
                 }
-                else
+                else if(c!=words.get(state).get(iterCh))
                 {
-                    System.out.println("FAILURE");
-                    wordExist = false;
-                    break;
+                    if(stateTransition.get(state).get(iterCh)!=null)
+                    {
+                        state = stateTransition.get(state).get(iterCh);
+                        iterCh = 0;
+                    }
+                    else if (stateTransition.get(state).get(iterCh) == null)
+                    {
+                        addTransition(state);//добавление посылания на продолжение нового слова
+                        addWord();//добавление оставшихся букв нового слова
+                        steps++;
+                        break;
+                    }
                 }
             }
         }
-        else{
+        else
+        {
             System.out.println("Dictionary is empty.");
             wordExist = false;
-        }
-
-        afterChecking();
-    }
-
-    private void afterChecking()
-    {
-        if(wordExist) System.out.println("Word exist.");
-        else if(!wordExist)
-        {
-            System.out.println("Word not exist.");
             addIfWanted();
         }
     }
 
+    private void addTransition(int state)
+    {
+        stateTransition.get(state).set(iterCh, words.size());
+    }
+
     private void addIfWanted()
     {
-        System.out.print("Do you want to add word? Y/N ");
+        System.out.println("Word not exist.");
+        System.out.print("Do you want to add? Y/N ");
         String tmp = sc.next();
-        if(tmp.toCharArray()[0]=='Y' || tmp.toCharArray()[0]=='y') addWord();
+        if (tmp.toCharArray()[0] == 'Y' || tmp.toCharArray()[0] == 'y')
+            addWord();
     }
 
     private void addWord()
     {
         //указание на другую последовательность
         ArrayList<Character> tmpList = new ArrayList<>();
-        for(char c:glTmpCh) tmpList.add(c);
+        ArrayList<Integer> tmpStList = new ArrayList<>();
+
+        for (int i = steps; i < glTmpCh.length; i++)
+        {
+            tmpList.add(glTmpCh[i]);
+            tmpStList.add(null);
+        }
         if(words.add(tmpList))System.out.println("Word added successfully.");
+        else System.out.println("Something went wrong.Words");
+
+        if(stateTransition.add(tmpStList)) System.out.println("States initiated successfully.");
+        else System.out.println("Something went wrong.States");
+
         showWords();
     }
 
     private void showWords()
     {
-        if(!words.isEmpty())
+        int tmpIter = 0;
+        lineI();
+        if (!words.isEmpty())
         {
+            System.out.println("Your dictionary.");
             for (ArrayList<Character> b : words)
+            {
+                System.out.println("[" + tmpIter + "]");
                 for (Character c : b)
+                {
                     System.out.println(c);
+                }
+                thinLineI();
+                tmpIter++;
+            }
+            lineI();
+            tmpIter = 0;
+            System.out.println("Your state dictionary.");
+            for (ArrayList<Integer> i : stateTransition)
+            {
+                System.out.println("[" + tmpIter + "]");
+                for (Integer a : i)
+                {
+                    if (a == null)
+                    {
+                        System.out.println("null");
+                    }
+                    else
+                    {
+                        System.out.println(a);
+                    }
+                }
+                thinLineI();
+                tmpIter++;
+            }
+            lineI();
         }
         else
         {
             System.out.println("Dictionary is empty.");
         }
     }
+
+    private void lineI()
+    {
+        System.out.println("======================");
+    }
+    private void thinLineI()
+    {
+        System.out.println("----------------------");
+    }
 }
+/*
+* EnterWord()
+        ArrayList<Integer> tmp = new ArrayList<>();
+
+        for (int i = 0; i < glTmpCh.length; i++) {
+            tmp.add(null);
+        }
+        stateTransition.add(tmp);*/
