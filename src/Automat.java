5df67f073e4ef32e05512de4ec4b60ee7de4cd43
Sora0Siro <43938296+Sora0Siro@users.noreply.github.com>
2018-10-17 03:55:52 +0300

    *Filter of wrong words *More developed user/console interaction Correction of mistakes in add and search =.=

--- src/Automat.java
+++ src/Automat.java
@@ -22,8 +22,7 @@ public class Automat
         stateTransition = new ArrayList<>();
         //
         do{
-        enterWord();
-        checkSequence();
+            enterWord();
             System.out.print("==============================\r" + "Continue? Y/N");
             String tmp = sc.next();
             if(tmp.toCharArray()[0]=='Y' || tmp.toCharArray()[0]=='y') cont=true;
@@ -38,7 +37,34 @@ public class Automat
         glTmp = null; glTmpCh = null;
         System.out.print("Enter your word: ");
         glTmp = sc.next();
-        glTmpCh = glTmp.toCharArray();
+        if(dividing())checkSequence();//только первое слов из всех возможно введнены0х
+    }
+
+    private boolean dividing()
+    {
+        int match = 0;//проверка условий на принадлежность каждого символа к букве?
+        String[] tokens = glTmp.split("\\*");//разделение всех возможных слов при вводе
+        if(!glTmp.isEmpty() && tokens.length!=0)
+        {
+            glTmp = tokens[0];
+            glTmpCh = glTmp.toCharArray();
+
+            for(int i =0;i<tokens[0].length();i++)
+            {
+                if(Character.isLetter(glTmpCh[i]))match++;
+            }
+        }
+        if(!glTmp.isEmpty() && tokens.length!=0 && match==glTmpCh.length)
+        {
+            glTmp +="*";
+            glTmpCh = glTmp.toCharArray();
+            return true;
+        }
+        else
+        {
+            System.out.println("Your word is not match the requirements.");
+            return false;
+        }
     }
 
     private void checkSequence()
@@ -46,28 +72,33 @@ public class Automat
         steps = 0;
         int state = 0;//номер проверяем. ячейки
         iterCh = 0;
+        //
         if(!words.isEmpty())//если словарь не пустой
         {
-            for(char c: glTmpCh)//каждый символ в введенном слове
+            for (int i = steps; i < glTmpCh.length; i++)//1;4
             {
-                if(c==words.get(state).get(iterCh))
+                if (glTmpCh[i] == words.get(state).get(iterCh))//true;true;true;false
                 {
                     wordExist = true;//проверяемые буквы одинаковы
                     iterCh++; //итерация для след. шага
-                    steps++;
+                    steps++;//кол-во совп. букв
                 }
-                else if(c!=words.get(state).get(iterCh))
+                else if (glTmpCh[i] != words.get(state).get(iterCh))
                 {
-                    if(stateTransition.get(state).get(iterCh)!=null)
+                    if (stateTransition.get(state).get(iterCh) != null)
                     {
                         state = stateTransition.get(state).get(iterCh);
                         iterCh = 0;
+                        i-=1;
                     }
                     else if (stateTransition.get(state).get(iterCh) == null)
                     {
-                        addTransition(state);//добавление посылания на продолжение нового слова
-                        addWord();//добавление оставшихся букв нового слова
-                        steps++;
+                        if(addIfWanted())
+                        {
+                            steps++;
+                            addTransition(state);//добавление посылания на продолжение нового
+                        }//добавление оставшихся букв нового слова
+
                         break;
                     }
                 }
@@ -77,22 +108,28 @@ public class Automat
         {
             System.out.println("Dictionary is empty.");
             wordExist = false;
-            addIfWanted();
+            if(addIfWanted()) wordExist = true;
         }
+        exOrNo();
+        showParallelWords();
     }
 
     private void addTransition(int state)
     {
-        stateTransition.get(state).set(iterCh, words.size());
+        stateTransition.get(state).set(iterCh, words.size()-1);
     }
 
-    private void addIfWanted()
+    private boolean addIfWanted()
     {
         System.out.println("Word not exist.");
         System.out.print("Do you want to add? Y/N ");
         String tmp = sc.next();
         if (tmp.toCharArray()[0] == 'Y' || tmp.toCharArray()[0] == 'y')
+        {
             addWord();
+            return true;
+        }
+        else return false;
     }
 
     private void addWord()
@@ -111,11 +148,38 @@ public class Automat
 
         if(stateTransition.add(tmpStList)) System.out.println("States initiated successfully.");
         else System.out.println("Something went wrong.States");
-
-        showWords();
     }
 
-    private void showWords()
+    private void showParallelWords()
+    {
+        lineI();
+        System.out.println("Parallel output of dictionary");
+        for(int i =0;i<words.size();i++)
+        {
+            System.out.println("[" + i + "]");
+            System.out.println(words.get(i) + "|" + stateTransition.get(i));
+        }
+        lineI();
+    }
+
+    private void lineI()
+    {
+        System.out.println("======================");
+    }
+
+    private void thinLineI()
+    {
+        System.out.println("----------------------");
+    }
+
+    private void exOrNo()
+    {
+        if(wordExist)System.out.println("Word exist.");
+        else if(!wordExist) System.out.println("Word not exist.");
+    }
+}
+
+/* private void showWords()
     {
         int tmpIter = 0;
         lineI();
@@ -158,22 +222,4 @@ public class Automat
         {
             System.out.println("Dictionary is empty.");
         }
-    }
-
-    private void lineI()
-    {
-        System.out.println("======================");
-    }
-    private void thinLineI()
-    {
-        System.out.println("----------------------");
-    }
-}
-/*
-* EnterWord()
-        ArrayList<Integer> tmp = new ArrayList<>();
-
-        for (int i = 0; i < glTmpCh.length; i++) {
-            tmp.add(null);
-        }
-        stateTransition.add(tmp);*/
+    }*/
